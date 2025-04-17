1ec685c5a472fb4a85d03ead75eb043c197b90df
Derviş Mehmet USTA <mehmet@Derviss-MacBook-Pro.local>
2025-04-17 10:54:46 +0300

    day 2  just started with new branch day2, today i gonna learn git branchig and finish basic go, then start to make a kanban app as a first app in go, then i gonna make my day scheduled every single day

--- t.go
+++ t.go
@@ -4,8 +4,17 @@ import "fmt"
 
 func main() {
 	var userName string
+	var newArray [50]string
+	newArray2 := []string{}
 	fmt.Println("Please enter your name")
 	fmt.Scan(&userName)
-	fmt.Printf("Your name is %s", userName)
+	fmt.Printf("Your name is %s \n", userName)
+	var bookings = [50]string{"Nana", "mehmet", "ece"}
+	fmt.Println(bookings[1])
+	newArray[0] = userName
+	fmt.Printf("for 1: %v ", newArray[0])
+	newArray2 = append(newArray[:], userName)
+	fmt.Printf("for 2: %v ", newArray2[:])
 
+	fmt.Printf("%T", newArray2[:])
 }
